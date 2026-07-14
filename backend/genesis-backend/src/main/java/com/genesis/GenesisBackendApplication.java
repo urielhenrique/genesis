492450ce5539f40585c5ff794a824657b6394a05
Uriel Gomes <uriel.henrique.gomes.uh@gmail.com>
2026-07-14 19:36:08 -0300

    build: bootstrap Spring Boot backend

---
+++ backend/genesis-backend/src/main/java/com/genesis/GenesisBackendApplication.java
@@ -0,0 +1,13 @@
+package com.genesis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenesisBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenesisBackendApplication.class, args);
+    }
+
+}
